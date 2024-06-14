fdf7954d23e6dfe73bd25c54012a2c3097c909cf
DeeChael <DeeChael@outlook.com>
2024-06-14 17:21:55 +0800

    feat: not depending on sodium anymore

---
+++ common/src/main/java/net/deechael/concentration/mixin/VideoSettingsScreenMixin.java
@@ -0,0 +1,55 @@
+package net.deechael.concentration.mixin;
+
+import net.deechael.concentration.Concentration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.OptionInstance;
+import net.minecraft.client.Options;
+import net.minecraft.client.gui.screens.options.VideoSettingsScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(VideoSettingsScreen.class)
+public class VideoSettingsScreenMixin {
+
+    @Inject(method = "options", at = @At("HEAD"), cancellable = true)
+    private static void inject$options(Options options, CallbackInfoReturnable<OptionInstance<?>[]> cir) {
+        cir.setReturnValue(
+                new OptionInstance[] {
+                        options.graphicsMode(),
+                        options.renderDistance(),
+                        options.prioritizeChunkUpdates(),
+                        options.simulationDistance(),
+                        options.ambientOcclusion(),
+                        options.framerateLimit(),
+                        options.enableVsync(),
+                        options.bobView(),
+                        options.guiScale(),
+                        options.attackIndicator(),
+                        options.gamma(),
+                        options.cloudStatus(),
+                        concentration$wrapperFullscreen(),
+                        options.particles(),
+                        options.mipmapLevels(),
+                        options.entityShadows(),
+                        options.screenEffectScale(),
+                        options.entityDistanceScaling(),
+                        options.fovEffectScale(),
+                        options.showAutosaveIndicator(),
+                        options.glintSpeed(),
+                        options.glintStrength(),
+                        options.menuBackgroundBlurriness()
+                }
+        );
+    }
+
+    @Unique
+    private static OptionInstance<Boolean> concentration$wrapperFullscreen() {
+        return OptionInstance.createBoolean("options.fullscreen", false, (value) -> {
+            Concentration.toggleFullScreenMode(Minecraft.getInstance().options, value);
+        });
+    }
+
+}
